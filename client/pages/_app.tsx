377c2529859bf3f7fefce5dd4ff5e5c4ed34f1fb
Levente Danel <levente.danel@gmail.com>
2021-09-03 19:41:41 +0300

    set up the frontend

---
+++ client/pages/_app.tsx
@@ -0,0 +1,12 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import React from "react";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <React.StrictMode>
+      <Component {...pageProps} />
+    </React.StrictMode>
+  );
+}
+export default MyApp;
