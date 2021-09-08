16f1ffcab68015d2b78ee9510d76d1f434704f89
Levente Danel <levente.danel@gmail.com>
2021-09-08 17:56:00 +0300

    change jest settings

--- client/jest.config.js
+++ client/jest.config.js
@@ -9,10 +9,21 @@ module.exports = {
   collectCoverageFrom: [
     "**/*.{js,jsx,ts,tsx}",
     "!**/*.d.ts",
+    "!**config.js",
+    "!**pages/_app.tsx",
+    "!**utils/**",
     "!**/node_modules/**",
     "!**.next/**",
     "!**coverage/**",
   ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
   moduleNameMapper: {
     // Handle CSS imports (with CSS modules)
     // https://jestjs.io/docs/webpack#mocking-css-modules
